671d6d75a3168cfbe116968956ea7feecd661e6f
Philippe Thierry <phil@reseau-libre.net>
2018-09-18 14:55:55 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,91 @@
+APP_NAME ?= usb
+DIR_NAME = usb
+
+PROJ_FILES = ../../
+BIN_NAME = $(APP_NAME).bin
+HEX_NAME = $(APP_NAME).hex
+ELF_NAME = $(APP_NAME).elf
+
+######### Metadata ##########
+ifeq ($(APP_NAME),usb)
+    IMAGE_TYPE = IMAGE_TYPE0
+else
+    IMAGE_TYPE = IMAGE_TYPE1
+endif
+
+VERSION = 1
+#############################
+
+-include $(PROJ_FILES)/Makefile.conf
+-include $(PROJ_FILES)/Makefile.gen
+
+# use an app-specific build dir
+APP_BUILD_DIR = $(BUILD_DIR)/apps/$(DIR_NAME)
+
+CFLAGS += -Isrc/ -Iinc/
+CFLAGS += $(APPS_CFLAGS)
+CFLAGS += -MMD -MP
+
+LDFLAGS += $(AFLAGS) -fno-builtin -nostdlib -nostartfiles -Xlinker -Map=$(APP_BUILD_DIR)/$(APP_NAME).map
+
+# FIXME: -lg should be replaced by local malloc/free
+LD_LIBS += -lmassstorage -lusb -lstd -ldebug 
+
+EXTRA_LDFLAGS ?= -Tusb.fw1.ld
+LDFLAGS += $(EXTRA_LDFLAGS) -L$(APP_BUILD_DIR)
+
+BUILD_DIR ?= $(PROJ_FILE)build
+
+CSRC_DIR = src
+SRC = $(wildcard $(CSRC_DIR)/*.c)
+OBJ = $(patsubst %.c,$(APP_BUILD_DIR)/%.o,$(SRC))
+DEP = $(OBJ:.o=.d)
+
+#Rust sources files
+RSSRC_DIR=rust/src
+RSRC= $(wildcard $(RSRCDIR)/*.rs)
+ROBJ = $(patsubst %.rs,$(APP_BUILD_DIR)/rust/%.o,$(RSRC))
+
+#ada sources files
+ASRC_DIR = ada/src
+ASRC= $(wildcard $(ASRC_DIR)/*.adb)
+AOBJ = $(patsubst %.adb,$(APP_BUILD_DIR)/ada/%.o,$(ASRC))
+
+OUT_DIRS = $(dir $(DRVOBJ)) $(dir $(BOARD_OBJ)) $(dir $(SOC_OBJ)) $(dir $(CORE_OBJ)) $(dir $(AOBJ)) $(dir $(OBJ)) $(dir $(ROBJ))
+
+LDSCRIPT_NAME = $(APP_BUILD_DIR)/$(APP_NAME).ld
+
+# file to (dist)clean
+# objects and compilation related
+TODEL_CLEAN += $(ROBJ) $(OBJ) $(SOC_OBJ) $(DRVOBJ) $(BOARD_OBJ) $(CORE_OBJ) $(DEP) $(TESTSDEP) $(SOC_DEP) $(DRVDEP) $(BOARD_DEP) $(CORE_DEP) $(LDSCRIPT_NAME)
+# targets
+TODEL_DISTCLEAN += $(APP_BUILD_DIR)
+
+.PHONY: app
+
+#############################################################
+# build targets (driver, core, SoC, Board... and local)
+
+all: $(APP_BUILD_DIR) app
+
+app: $(APP_BUILD_DIR)/$(ELF_NAME) $(APP_BUILD_DIR)/$(HEX_NAME)
+
+$(APP_BUILD_DIR)/%.o: %.c
+	$(call if_changed,cc_o_c)
+
+
+# ELF
+$(APP_BUILD_DIR)/$(ELF_NAME): $(OBJ)
+	$(call if_changed,link_o_target)
+
+# HEX
+$(APP_BUILD_DIR)/$(HEX_NAME): $(APP_BUILD_DIR)/$(ELF_NAME)
+	$(call if_changed,objcopy_ihex)
+
+# BIN
+$(APP_BUILD_DIR)/$(BIN_NAME): $(APP_BUILD_DIR)/$(ELF_NAME)
+	$(call if_changed,objcopy_bin)
+
+$(APP_BUILD_DIR):
+	$(call cmd,mkdir)
+
---
+++ src/main.c
@@ -0,0 +1,166 @@
+/**
+ * @file main.c
+ *
+ * \brief Main of dummy
+ *
+ */
+
+#include "api/syscall.h"
+#include "api/print.h"
+#include "ipc_proto.h"
+#include "usb.h"
+#include "usb_control.h"
+#include "scsi.h"
+#include "masstorage.h"
+#include "api/malloc.h"
+
+static void my_irq_handler(void);
+
+char buffer_out[16] = "[five guys!   ]\0";
+char buffer_in[16] = "               \0";
+
+uint32_t num_tim = 0;
+
+void tim_handler(uint8_t irq)
+{
+    irq = irq;
+    num_tim++;
+}
+
+uint8_t usb_buf[4096];
+
+/*
+ * We use the local -fno-stack-protector flag for main because
+ * the stack protection has not been initialized yet.
+ */
+int _main(uint32_t task_id)
+{
+//    const char * test = "hello, I'm usb\n";
+    e_syscall_ret ret = 0;
+//    uint32_t size = 256;
+    uint8_t id_crypto = 0;
+    uint8_t id;
+    struct sync_command ipc_sync_cmd;
+    dma_shm_t dmashm_rd;
+    dma_shm_t dmashm_wr;
+#if 0
+    int i = 0;
+    uint64_t tick = 0;
+#endif
+
+    printf("Hello ! I'm usb, my id is %x\n", task_id);
+
+    ret = sys_init(INIT_GETTASKID, "crypto", &id_crypto);
+    printf("crypto is task %x !\n", id_crypto);
+
+    /*********************************************
+     * Declaring DMA Shared Memory with Crypto
+     *********************************************/
+    dmashm_rd.target = id_crypto;
+    dmashm_rd.source = task_id;
+    dmashm_rd.address = (physaddr_t)usb_buf;
+    dmashm_rd.size = 4000;
+    /* Crypto DMA will read from this buffer */
+    dmashm_rd.mode = DMA_SHM_ACCESS_RD;
+
+    dmashm_wr.target = id_crypto;
+    dmashm_wr.source = task_id;
+    dmashm_wr.address = (physaddr_t)usb_buf;
+    dmashm_wr.size = 4000;
+    /* Crypto DMA will write into this buffer */
+    dmashm_wr.mode = DMA_SHM_ACCESS_WR;
+
+    printf("Declaring DMA_SHM for SDIO read flow\n");
+    ret = sys_init(INIT_DMA_SHM, &dmashm_rd);
+    printf("sys_init returns %s !\n", strerror(ret));
+
+    printf("Declaring DMA_SHM for SDIO write flow\n");
+    ret = sys_init(INIT_DMA_SHM, &dmashm_wr);
+    printf("sys_init returns %s !\n", strerror(ret));
+
+    /* initialize the SCSI stack with two buffers of 4096 bits length each. */
+    scsi_early_init(usb_buf, 4096);
+
+    /*******************************************
+     * End of init
+     *******************************************/
+
+    ret = sys_init(INIT_DONE);
+    printf("sys_init DONE returns %x !\n", ret);
+
+
+    /*******************************************
+     * let's syncrhonize with other tasks
+     *******************************************/
+    logsize_t size = 2;
+
+    printf("sending end_of_init syncrhonization to crypto\n");
+    ipc_sync_cmd.magic = MAGIC_TASK_STATE_CMD;
+    ipc_sync_cmd.state = SYNC_READY;
+
+    do {
+      ret = sys_ipc(IPC_SEND_SYNC, id_crypto, size, (const char*)&ipc_sync_cmd);
+    } while (ret != SYS_E_DONE);
+
+    /* Now wait for Acknowledge from Smart */
+    id = id_crypto;
+
+    do {
+        ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
+    } while (ret != SYS_E_DONE);
+    if (   ipc_sync_cmd.magic == MAGIC_TASK_STATE_RESP
+        && ipc_sync_cmd.state == SYNC_ACKNOWLEDGE) {
+        printf("crypto has acknowledge end_of_init, continuing\n");
+    }
+
+    /*******************************************
+     * Starting end_of_cryp synchronization
+     *******************************************/
+
+    printf("waiting end_of_cryp syncrhonization from crypto\n");
+
+    id = id_crypto;
+    size = 2;
+
+    do {
+        ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_sync_cmd);
+    } while (ret != SYS_E_DONE);
+
+    if (   ipc_sync_cmd.magic == MAGIC_TASK_STATE_CMD
+        && ipc_sync_cmd.state == SYNC_READY) {
+        printf("crypto module is ready\n");
+    }
+
+    /* Initialize USB device */
+    wmalloc_init();
+    scsi_init();
+    mass_storage_init();
+
+    ipc_sync_cmd.magic = MAGIC_TASK_STATE_RESP;
+    ipc_sync_cmd.state = SYNC_READY;
+
+    size = 2;
+    do {
+      ret = sys_ipc(IPC_SEND_SYNC, id_crypto, size, (char*)&ipc_sync_cmd);
+    } while (ret != SYS_E_DONE);
+
+    /*******************************************
+     * Starting USB listener
+     *******************************************/
+
+    printf("USB main loop starting\n");
+
+    scsi_state_machine();
+
+    while (1) {
+        sys_yield();
+    }
+    /* should return to do_endoftask() */
+    return 0;
+}
+
+/* this is the uart IRQ handler for UART implem */
+static void my_irq_handler(void)
+{
+
+}
