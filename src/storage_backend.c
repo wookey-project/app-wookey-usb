5c664592e99693774fd1f54fba2164385df3702a
Philippe Thierry <phil@reseau-libre.net>
2019-04-03 15:40:17 +0200

    [enhancement] cleaning backend handlers. Add support for read/write error recovery

--- src/storage_backend.c
+++ src/storage_backend.c
@@ -10,9 +10,10 @@
 extern uint8_t id_crypto;
 
 
-uint8_t scsi_storage_backend_capacity(uint32_t *numblocks, uint32_t *blocksize)
+mbed_error_t scsi_storage_backend_capacity(uint32_t *numblocks, uint32_t *blocksize)
 {
 
+    mbed_error_t error_code = MBED_ERROR_NONE;
     #if USB_APP_DEBUG
         printf("%s\n", __func__);
     #endif
@@ -26,33 +27,32 @@ uint8_t scsi_storage_backend_capacity(uint32_t *numblocks, uint32_t *blocksize)
     memset((void*)&ipc_sync_cmd_data, 0, sizeof(struct sync_command_data));
 
     ipc_sync_cmd_data.magic = MAGIC_STORAGE_SCSI_BLOCK_NUM_CMD;
-    do {
-        ret = sys_ipc(IPC_SEND_SYNC, sinker, sizeof(struct sync_command), (char*)&ipc_sync_cmd_data);
-        if (ret != SYS_E_DONE) {
-            # if USB_APP_DEBUG
-                printf("%s:%d Oops ! ret = %d\n", __func__, __LINE__, ret);
-            #endif
-        } else {
-            # if USB_APP_DEBUG
-                printf("%s:%d IPC MAGIC_STORAGE_SCSI_BLOCK_SIZE_CMD succesfully sent.\n", __func__, __LINE__);
-            #endif
-        }
-    } while (ret != SYS_E_DONE);
+    ret = sys_ipc(IPC_SEND_SYNC, sinker, sizeof(struct sync_command), (char*)&ipc_sync_cmd_data);
+    if (ret != SYS_E_DONE) {
+# if USB_APP_DEBUG
+        printf("%s:%d Oops ! ret = %d\n", __func__, __LINE__, ret);
+#endif
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    } else {
+# if USB_APP_DEBUG
+        printf("%s:%d IPC MAGIC_STORAGE_SCSI_BLOCK_SIZE_CMD succesfully sent.\n", __func__, __LINE__);
+#endif
+    }
 
 
-    do {
-        ret = sys_ipc(IPC_RECV_SYNC, &sinker, &size, (char*)&ipc_sync_cmd_data);
-        if (ret != SYS_E_DONE) {
-        # if USB_APP_DEBUG
-                printf("%s:%d Oops ! ret = %d\n", __func__, __LINE__, ret);
-        #endif
-        } else {
-        # if USB_APP_DEBUG
-            printf("%s:%d Got IPC_RECV_SYNC.\n");
-        #endif
-
-        }
-    } while (ret != SYS_E_DONE);
+    ret = sys_ipc(IPC_RECV_SYNC, &sinker, &size, (char*)&ipc_sync_cmd_data);
+    if (ret != SYS_E_DONE) {
+# if USB_APP_DEBUG
+        printf("%s:%d Oops ! ret = %d\n", __func__, __LINE__, ret);
+#endif
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    } else {
+# if USB_APP_DEBUG
+        printf("%s:%d Got IPC_RECV_SYNC.\n");
+#endif
+    }
 
     if (ipc_sync_cmd_data.magic == MAGIC_STORAGE_SCSI_BLOCK_NUM_RESP) {
         //block_size = ipc_sync_cmd_data.data.u32[0];
@@ -60,96 +60,114 @@ uint8_t scsi_storage_backend_capacity(uint32_t *numblocks, uint32_t *blocksize)
         # if USB_APP_DEBUG
             printf("%s:%d Received storage_capacity is %d\n",  __func__, __LINE__, *numblocks);
         #endif
+    } else {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
     }
 
     memset((void*)&ipc_sync_cmd_data, 0, sizeof(struct sync_command_data));
     ipc_sync_cmd_data.magic = MAGIC_STORAGE_SCSI_BLOCK_SIZE_CMD;
 
-    do {
 
-        do {
-            ret = sys_ipc(IPC_SEND_SYNC, sinker, sizeof(struct sync_command), (char*)&ipc_sync_cmd_data);
-            if (ret != SYS_E_DONE) {
+    ret = sys_ipc(IPC_SEND_SYNC, sinker, sizeof(struct sync_command), (char*)&ipc_sync_cmd_data);
+    if (ret != SYS_E_DONE) {
 # if USB_APP_DEBUG
-                printf("%s:%d Oops ! ret = %d\n",  __func__, __LINE__, ret);
+        printf("%s:%d Oops ! ret = %d\n",  __func__, __LINE__, ret);
 #endif
-            } else {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    } else {
 # if USB_APP_DEBUG
-                printf("%s:%d IPC MAGIC_STORAGE_SCSI_BLOCK_SIZE_CMD succesfully sent.\n", __func__, __LINE__);
+        printf("%s:%d IPC MAGIC_STORAGE_SCSI_BLOCK_SIZE_CMD succesfully sent.\n", __func__, __LINE__);
 #endif
-            }
-        } while(ret!= SYS_E_DONE);
+    }
 
-        do {
-            if (ret == SYS_E_DONE) {
-                ret = sys_ipc(IPC_RECV_SYNC, &sinker, &size, (char*)&ipc_sync_cmd_data);
-                if (ret != SYS_E_DONE) {
+    ret = sys_ipc(IPC_RECV_SYNC, &sinker, &size, (char*)&ipc_sync_cmd_data);
+    if (ret != SYS_E_DONE) {
 # if USB_APP_DEBUG
-                    printf("%s:%d Oops ! ret = %d\n",  __func__, __LINE__, ret);
+        printf("%s:%d Oops ! ret = %d\n",  __func__, __LINE__, ret);
 #endif
-                } else {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    } else {
 # if USB_APP_DEBUG
-                    printf("%s:%d Got IPC_RECV_SYNC.\n", __func__, __LINE__);
+        printf("%s:%d Got IPC_RECV_SYNC.\n", __func__, __LINE__);
 #endif
-                }
-                if (ret != SYS_E_DONE) {
-                    goto error;
-                }
-            }
-        } while(ret!= SYS_E_DONE);
+    }
 
-    } while (ipc_sync_cmd_data.magic != MAGIC_STORAGE_SCSI_BLOCK_SIZE_RESP);
 
 
     *blocksize = ipc_sync_cmd_data.data.u32[0];
     # if USB_APP_DEBUG
         printf("%s:%d Received block size is %d\n", __func__,  __LINE__, *blocksize);
     #endif
-    return 0;
+    return error_code;
 
 error:
     # if USB_APP_DEBUG
         printf("%s:%d ERROR: getting block size from lower layers ...\n", __func__,  __LINE__);
     #endif
-    return 1;
+    return error_code;
 
 }
 
 
-uint8_t scsi_storage_backend_read(uint32_t sector_address,
-                                  uint32_t num_sectors)
+mbed_error_t  scsi_storage_backend_read(uint32_t sector_address,
+                                        uint32_t num_sectors)
 {
 
     struct dataplane_command dataplane_command_rd = { 0 };
     struct dataplane_command dataplane_command_ack = { 0 };
     uint8_t sinker = id_crypto;
+    uint8_t ret;
+    mbed_error_t error_code = MBED_ERROR_NONE;
+
     logsize_t ipcsize = sizeof(struct dataplane_command);
 
     dataplane_command_rd.magic = MAGIC_DATA_RD_DMA_REQ;
     dataplane_command_rd.sector_address = sector_address;
     dataplane_command_rd.num_sectors = num_sectors;
     // ipc_dma_request to cryp
-    sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_rd);
+    ret = sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_rd);
+    if (ret != SYS_E_DONE) {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    }
     sinker = id_crypto;
     ipcsize = sizeof(struct dataplane_command);
-    sys_ipc(IPC_RECV_SYNC, &sinker, &ipcsize, (char*)&dataplane_command_ack);
+    ret = sys_ipc(IPC_RECV_SYNC, &sinker, &ipcsize, (char*)&dataplane_command_ack);
+    if (ret != SYS_E_DONE) {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    }
     if (dataplane_command_ack.magic != MAGIC_DATA_RD_DMA_ACK) {
         printf("dma request to sinker didn't received acknowledge\n");
-        return 1;
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    }
+    if (dataplane_command_ack.state != SYNC_ACKNOWLEDGE) {
+        if (dataplane_command_ack.state == SYNC_FAILURE) {
+            error_code = MBED_ERROR_RDERROR;
+        } else {
+            error_code = MBED_ERROR_UNKNOWN;
+        }
     }
     #if USB_APP_DEBUG
         printf("==> storage_read10_data 0x%x %d\n", dataplane_command_rd.sector_address, num_sectors);
     #endif
-    return 0;
+error:
+    return error_code;
 }
 
-uint8_t scsi_storage_backend_write(uint32_t sector_address,
-                                   uint32_t num_sectors)
+mbed_error_t scsi_storage_backend_write(uint32_t sector_address,
+                                        uint32_t num_sectors)
 {
     struct dataplane_command dataplane_command_wr = { 0 };
     struct dataplane_command dataplane_command_ack = { 0 };
     uint8_t sinker = id_crypto;
     logsize_t ipcsize = sizeof(struct dataplane_command);
+    uint8_t ret;
+    mbed_error_t error_code = MBED_ERROR_NONE;
 
     dataplane_command_wr.magic = MAGIC_DATA_WR_DMA_REQ;
     dataplane_command_wr.sector_address = sector_address;
@@ -158,19 +176,36 @@ uint8_t scsi_storage_backend_write(uint32_t sector_address,
 #if USB_APP_DEBUG
     printf("requesting %x num sectors, addr 0x%08x\n", num_sectors, sector_address);
 #endif
-    sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_wr);
+
+    ret = sys_ipc(IPC_SEND_SYNC, id_crypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_wr);
+    if (ret != SYS_E_DONE) {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    }
     sinker = id_crypto;
     ipcsize = sizeof(struct dataplane_command);
-    sys_ipc(IPC_RECV_SYNC, &sinker, &ipcsize, (char*)&dataplane_command_ack);
+    ret = sys_ipc(IPC_RECV_SYNC, &sinker, &ipcsize, (char*)&dataplane_command_ack);
+    if (ret != SYS_E_DONE) {
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
+    }
     if (dataplane_command_ack.magic != MAGIC_DATA_WR_DMA_ACK) {
         printf("dma request to sinker didn't received acknowledge\n");
-        return 1;
+        error_code = MBED_ERROR_NOSTORAGE;
+        goto error;
     }
-
+    if (dataplane_command_ack.state != SYNC_ACKNOWLEDGE) {
+        if (dataplane_command_ack.state == SYNC_FAILURE) {
+            error_code = MBED_ERROR_WRERROR;
+        } else {
+            error_code = MBED_ERROR_UNKNOWN;
+        }
+    }
     #if USB_APP_DEBUG
         printf("==> storage_write10_data 0x%x %d\n", dataplane_command_wr.sector_address, num_sectors);
     #endif
-    return 0;
+error:
+    return error_code;
 }
 
 
